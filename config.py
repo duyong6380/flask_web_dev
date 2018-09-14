ffcca763854fb4ac546ebed5ac9f6126e149a4c3
duyong6380 <duyong6380@163.com>
2018-09-14 03:06:46 -0700

    分页导航

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ class Config:
     FLASKY_MAIL_SENDER = 'duyong<326725783@qq.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FLASKY_POSTS_PER_PAGE = 10
 
     @staticmethod
     def init_app(app):
